e4eac9988d93de0e1cabd5ccd66e4d57bd5672b8
hernangiova <rhernandezromero@byupathway.edu>
2026-09-11 15:49:56 -0600

    journal assignment done

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,47 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFiles(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date} | {entry._promptText} | {entry._entryText}");
+            }
+        }
+    }
+    
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach(string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            string date = parts[0];
+            string prompt = parts[1];
+            string response = parts[2];
+
+            Entry entry = new Entry(date, prompt, response);
+            _entries.Add(entry);
+        }
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+
+
+        List<string> prompts = new List<string>()
+        {
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?"
+        };
+
+        Random random = new Random();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = prompts[random.Next(prompts.Count)];
+
+                Console.WriteLine(prompt);
+                Console.WriteLine("> ");
+                string response = Console.ReadLine();
+
+                string date = DateTime.Now.ToShortDateString();
+
+                Entry newEntry = new Entry(date, prompt, response);
+
+                journal.AddEntry(newEntry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                Console.WriteLine("Journal saved successfully.");
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                Console.WriteLine("Journal loaded successfully.");
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+
+        }
     }
 }
